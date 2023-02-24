b96926f6f153933a1bdb4bb4bd2f3b082223f68c
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2023-02-24 09:40:21 +0300

    task3: for frontend application: connect front with backend endpoint to receive list of products

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Kanstantsin Store!
           </Link>
         </Typography>
 
