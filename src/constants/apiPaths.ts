f3db0ff254acb5fa90bb7af09633966b26328c21
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2023-03-20 21:46:29 +0300

    changes after redeploy product-service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://czckjz2ev2.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://czckjz2ev2.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ul9t5uqsgb.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://czckjz2ev2.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://czckjz2ev2.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
