0c313ca57aba2fc2dfb41da5fccccebef77f154c
Kanstantsin Yarmolenka <Kanstantsin_Yarmolenka@epam.com>
2023-04-10 23:59:07 +0300

    task-8 init commit

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://ul9t5uqsgb.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://b11m8qz31c.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://xrpm0lagr2.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
